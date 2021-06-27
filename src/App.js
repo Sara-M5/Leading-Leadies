5a9be54b1dec21f3748445e79b6d73d3528810e8
sajadevo <nawabisajadahmad@gmail.com>
2021-06-27 11:58:22 +0430

    Remove the themes routes

--- src/App.js
+++ src/App.js
@@ -1,49 +1,41 @@
-
 import React, { Component } from 'react';
 import {
-  BrowserRouter as Router, Route, Redirect, Switch 
- } 
-from 'react-router-dom';
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 
 import './App.css';
 import Navbar from './Component/Navbar/Navbar';
 
-
 import Home from './Home';
 import About from './Pages/About/About';
 import Contact from './Pages/Contact/Contact';
-import Themes from './Pages/Themes/Themes';
 
 class App extends Component {
+  render() {
+    return (
+      <Router>
+        <main>
+          <Navbar />
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/About" exact>
+              <About />
+            </Route>
 
-render(){
-  
-return(
-     
-          
+            <Route path="/Contact" exact>
+              <Contact />
+            </Route>
 
-         
-            <Router>
-              <main>
-              <Navbar />
-                <Switch>
-              
-                 <Route path="/" exact ><Home /></Route>
-                 <Route path="/About" exact ><About /></Route>
-                 <Route path="/Themes" exact ><Themes /></Route>
-                 
-                 <Route path="/Contact" exact ><Contact /></Route>
-
-                 <Redirect to="/" />
-                </Switch>
-                </main>
-            </Router>
-
- 
-
+            <Redirect to="/" />
+          </Switch>
+        </main>
+      </Router>
     );
-
   }
-
 }
 export default App;
