a8e6771c296e92fcaddd7ce78e76d37e845fed90
Sara mahmoodi <74267458+Sara-M5@users.noreply.github.com>
2021-06-30 23:46:02 +0430

    Update Service.js

--- src/Pages/Pages/Services/Service.js
+++ src/Pages/Pages/Services/Service.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import DigiMarkets from '../../../Component/DigiMarket/DigiMarkets';
+import Header from '../../../Parts/Header';
+import LastPart from '../../../Parts/LastPart/LastPart';
 
 const Service = () => {
-
+  return(
     <div className="Container">
 
-    <h1 >Service</h1>
+      <Header 
+       title="Services"
+       subTitle="Services"
+       
+       />  
+
+    <DigiMarkets />
+    <LastPart />
+
 
     </div>
+    );
 }
 export default Service;
