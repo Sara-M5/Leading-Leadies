14b6ba031927f2c48fa0654746d91f9daab9561f
Sara mahmoodi <74267458+Sara-M5@users.noreply.github.com>
2021-06-30 23:45:03 +0430

    Update PricingPlan.js

--- src/Pages/Pages/Pricing Plan/PricingPlan.js
+++ src/Pages/Pages/Pricing Plan/PricingPlan.js
@@ -1,11 +1,50 @@
 import React from 'react';
+import PricingPlans from '../../../Component/PricingPlan/PricingPlans';
+import Header from '../../../Parts/Header';
+import LastPart from '../../../Parts/LastPart/LastPart';
+
 
 const PricingPlan = () => {
-
+    return(
+        
     <div className="Container">
+    
+    <Header 
+       title="Pricing Plan"
+       subTitle="Pricing Plan"
+       
+       />
+    
+   
+   
+    <div className="row text-center">
+        
+              <PricingPlans
+                 title="Basic"
+                 images="../images/pricing1.JPG"
+                 sign="$"
+                 Price="59"
+                  />
 
-    <h1 >PricingPlan</h1>
+              <PricingPlans 
+                 title="Standard"
+                 images="../images/pricing2.JPG"
+                 sign="$"
+                 Price= "59"
+                  />
 
+              <PricingPlans 
+         
+                 title="Premium"
+                 images="../images/pricing3.JPG"
+                 sign="$"
+                 Price="120"
+                 />
+         </div>
+      
+         <LastPart />
+    
     </div>
+    );
 }
 export default PricingPlan;
