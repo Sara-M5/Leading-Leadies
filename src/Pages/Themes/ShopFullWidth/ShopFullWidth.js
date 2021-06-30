a093afeadeee085e555b958cdef9f1fcc1818d6f
Sara mahmoodi <74267458+Sara-M5@users.noreply.github.com>
2021-07-01 00:44:17 +0430

    Update ShopFullWidth.js

--- src/Pages/Themes/ShopFullWidth/ShopFullWidth.js
+++ src/Pages/Themes/ShopFullWidth/ShopFullWidth.js
@@ -15,19 +15,18 @@ return (
        
        />
         <div className="container">
-<div className="filter-area">
-<div className="row pt-4">
-<div className="col-md-9 col-sm-6 my-auto">
-<p className="result-count ">
-Showing 1&ndash;9 of 40 results</p>
-</div>
-<div className="col-md-3 col-sm-6 my-auto">
+                <div className="filter-area">
+                <div className="row pt-4">
+                <div className="col-md-9 col-sm-6 my-auto">
+                <p className="result-count ">Showing 1&ndash;9 of 40 results</p>
+                </div>
 
-<div class="dropdown ordering">
-            <ul  className="dropdown-toggle result-count py-3 px-5 " data-toggle="dropdown">
-                Default sorting    <span className="sr-only"> </span>   
-            </ul>
-            <div class="dropdown-menu">
+                <div className="col-md-3 col-sm-6 my-auto">
+                <div class="dropdown ordering">
+                <ul  className="dropdown-toggle result-count py-3 px-5 " data-toggle="dropdown">
+                Default sorting<span className="sr-only"> </span> </ul>
+            
+                <div class="dropdown-menu">
                 <a class="dropdown-item" href="#">Default sorting</a>
                 <a class="dropdown-item" href="#">Sort by popularity</a>
                 <a class="dropdown-item" href="#">Sort by average rating</a>
@@ -37,36 +36,24 @@ Showing 1&ndash;9 of 40 results</p>
             </div>
         </div>
     
-</div>
-</div>
-</div>
-</div>
+        </div>
+        </div>
+        </div>
+        </div>
 
     
-    <Product />
-   <div className="row">
+                <Product />
+                <div className="row">
    
-            <ul className="pagination justify-content-center my-5">
+                <ul className="pagination justify-content-center my-5">
                 <span className="sr-only"></span>
-                <li className="page-item">
-                    <a className="page-link first active" href="#">1</a>
-                </li>
-                <li className="page-item">
-                    <a className="page-link" href="#">2</a>
-                </li>
-                <li className="page-item">
-                    <a className="page-link" href="#">3</a>
-                </li>
-                <li className="page-item">
-                    <span className="page-link">...</span>
-                </li>
-                <li className="page-item">
-                    <a className="page-link" href="#">4</a>
-                </li>
-                <li className="page-item">
-                    <a className="page-link" href="#">Next &#10095;</a>
-                </li>
-                    <span className="sr-only"></span>
+                <li className="page-item"><a className="page-link first active" href="#">1</a> </li>
+                <li className="page-item"><a className="page-link" href="#">2</a> </li>
+                <li className="page-item"><a className="page-link" href="#">3</a> </li>
+                <li className="page-item"><span className="page-link">...</span> </li>
+                <li className="page-item"><a className="page-link" href="#">4</a></li>
+                <li className="page-item"><a className="page-link" href="#">Next &#10095;</a> </li>
+                <span className="sr-only"></span>
                 
              
             </ul>
