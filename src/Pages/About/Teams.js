71872c99661e1d92285fa7d9820bb69c5cc7f47e
Sara mahmoodi <74267458+Sara-M5@users.noreply.github.com>
2021-06-30 23:37:08 +0430

    Add files via upload

---
+++ src/Pages/About/Teams.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Teams = (props) => {
+return(
+
+     <div className="col-sm-4 my-5">
+     <img className="Picture" src={props.image} alt=""/>
+     <div class="tijarah-team-content p-4">
+     <h4>{props.name}</h4>
+     <span>{props.job}</span>
+     </div>
+     </div>
+);
+}
+
+
+export default Teams;
