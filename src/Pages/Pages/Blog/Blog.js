59fface56e111ea94062236b85e13a059818e5f8
Sara mahmoodi <74267458+Sara-M5@users.noreply.github.com>
2021-06-30 23:30:36 +0430

    Update Blog.js

--- src/Pages/Pages/Blog/Blog.js
+++ src/Pages/Pages/Blog/Blog.js
@@ -1,11 +1,56 @@
 import React from 'react';
-
+import Post from '../../../Component/Post/Post';
+import BlogSidebar from './BlogSidebar';
+import Header from '../../../Parts/Header';
+import LastPart from '../../../Parts/LastPart/LastPart';
 const Blog = () => {
      return(
-    <div className="Container">
+    <div >
+       <Header 
+       title="Latest news"
+       subTitle="Blog"
+       
+       />
+       <div className="row">
+    
+           <div className="col-md-8 col-sm-4">
+          
+              <Post
+               images="../images/6.JPG"
+               title="The best advice to start your own project"
+               label="Octobar 29, 2018"
+               />
 
-    <h1 >Blog</h1>
+               <Post
+               images="../images/9.JPG"
+               title="Top Web Design Trends You Must Know in 2021"
+               label="June 12, 2018"
+               />
 
+
+              
+
+               <Post
+               images="../images/2.JPG"
+               title="Best Free Responsive WordPress Themes in 2020"
+               label="May 11, 2018"
+               />
+               
+
+           </div>
+           <div className="col-md-4 my-5">  
+         
+              
+                <div className="row">
+                   <BlogSidebar />
+                </div>
+
+            
+           </div>
+       </div>
+
+  
+    <LastPart />
     </div>
     );
 }
