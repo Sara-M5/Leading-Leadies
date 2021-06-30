8b50509648376e4916c559c3fde9a3da122d56e5
Sara mahmoodi <74267458+Sara-M5@users.noreply.github.com>
2021-06-30 23:47:00 +0430

    Update Team.js

--- src/Pages/Pages/Team/Team.js
+++ src/Pages/Pages/Team/Team.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import Header from '../../../Parts/Header';
+import LastPart from '../../../Parts/LastPart/LastPart';
+import Teams from '../../About/Teams';
+
 
 const Team = () => {
-
+    return (
     <div className="Container">
 
-    <h1 >Team</h1>
+    <Header 
+       title="Team"
+       subTitle="Team"
+       
+       />
+    
+     
+    <div className="row mb-5 py-4">
+         <Teams 
+         image="images/3(1).JPG"
+         name="Sanam Hamza"
+         job="Web Developer"
+         />
 
+        <Teams
+         image="images/team2.jpg"
+         name="Sara Mahmoodi"
+         job="Web Developer"
+         />
+
+        <Teams
+         image="images/3(3).JPG"
+         name="Zohra Baraki"
+         job="Web Developer"
+         />
+    
+     </div>
+    <LastPart />
+   
     </div>
+    );
 }
 export default Team;
