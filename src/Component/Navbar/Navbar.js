da545ba99ece82dc0e3e4b2c960322f42bca97a1
Sara mahmoodi <74267458+Sara-M5@users.noreply.github.com>
2021-07-06 00:12:38 +0430

    Update Navbar.js

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -86,7 +86,20 @@ const Navbar = () => {
                 Contact
               </NavLink>
             </li>
+            <li className="nav-item">
+              <NavLink className="cart-contents nav-link" to="#" title="View your shopping cart">
+              <span class="cart-contents-count"><i className="mx-2 fa fa-shopping-cart"></i>
+                0 items </span>
+              </NavLink>
+            </li>
           </ul>
+        </div>
+      </div>
+      <div className="col-xl-2">
+        <div className="header-btn d-xl-block">
+            <NavLink  to="/Login" className="my-account-btn" exact>
+            <img src="./images/user.png" alt="My account" />
+             My Account </NavLink>
         </div>
       </div>
     </nav>
