7d32e513c3bbfa0562d26d54ec75e0b7759075f8
Sara mahmoodi <74267458+Sara-M5@users.noreply.github.com>
2021-09-14 23:03:16 +0430

    added Data file

---
+++ src/Parts/ClientFeedback/Data.js
@@ -0,0 +1,24 @@
+export const Data = [
+
+   { 
+   images: "../images/3(1).JPG",
+   text: "Awesome Product highly recomended Lorem ipsum dolor alamet, nsectetur mayalipol tempor eiusmod tempor recomended Lorem ipsum dolor alamet, nsec tetur mayalipol tempor eiusmod tempor incubto ectetur alasiqua enim ad nim veniam, quis nostrud ullam ",
+   name: "Nancoda Mao",
+   job: "Ui/Ux Designer And Product Designer "
+   },
+
+   {
+   images: "../images/3(3).JPG",
+   text: "Awesome Product highly recomended Lorem ipsum dolor alamet, nsectetur mayalipol tempor eiusmod tempor recomended Lorem ipsum dolor alamet, nsec tetur mayalipol tempor eiusmod tempor incubto ectetur alasiqua enim ad nim veniam, quis nostrud ullam ",
+   name: "Sara Mahmoodi",
+   job: "Ui/Ux Designer And Product Designer "
+   },
+
+   { 
+   images: "../images/3(2).JPG",
+   text: "Awesome Product highly recomended Lorem ipsum dolor alamet, nsectetur mayalipol tempor eiusmod tempor recomended Lorem ipsum dolor alamet, nsec tetur mayalipol tempor eiusmod tempor incubto ectetur alasiqua enim ad nim veniam, quis nostrud ullam ",
+   name: "Emaley mcchulloch",
+   job: "Ui/Ux Designer And Product Designer "
+   }
+
+]
