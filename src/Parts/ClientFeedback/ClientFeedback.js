0f2608c22895f296bbb9406c39264f702d005ad4
Sara mahmoodi <74267458+Sara-M5@users.noreply.github.com>
2021-09-16 05:38:13 +0430

    Update ClientFeedback.js

--- src/Parts/ClientFeedback/ClientFeedback.js
+++ src/Parts/ClientFeedback/ClientFeedback.js
@@ -29,17 +29,30 @@ const ClientFeedback = ({slides}) => {
                 <div className={index === current ? 'slide active' : 'slide'} key={index}>
                     {index === current && ( 
                   
-                    <div className="d-flex justify-content-between">
-                    <div className="col1" >
+                    <div className="row">
+                    <div className="col-sm-12 col-lg-5 mt-5" >
                        <img className="circle" src={slide.images} alt="client-images" />
                     </div>
-                    <div className="col2">
-                         {slide.text}   
-                         <h3>{slide.name}</h3>
-                         {slide.job}
+                    <div className="col2 col-sm-0 col-lg-7 mt-5">
+                         <i className="quote fas fa-quote-left"></i>
+                         <p>{slide.text}</p> 
+                         <div class="d-flex justify-content-between">
+                            <div class="client-info">
+                            <h4>{slide.name}</h4>
+                            <span>{slide.job}</span>
+                            </div>
+                           
+                            <span className="mt-5">   
+                            <i style={{color:"gold"}} className="fas fa-star"></i>
+                            <i style={{color:"gold"}} className="fas fa-star px-1"></i>
+                            <i style={{color:"gold"}} className="fas fa-star "></i>
+                            <i style={{color:"gold"}} className="fas fa-star px-1"></i>
+                            <i style={{color:"gold"}} className="fas fa-star"></i> 
+                            </span>
+                         </div>
                          <div className="mt-3">
-                         <button onClick={prevSlide} className="p-4"><i className="fas fa-arrow-circle-left"></i></button>
-                         <button onClick={nextSlide} className="p-4"><i className="fas fa-arrow-circle-right" ></i></button> 
+                         <button onClick={prevSlide} className="btn-arrow px-3 py-2"><i className="fas fa-long-arrow-alt-left"></i></button>
+                         <button onClick={nextSlide} className="btn-arrow px-3 py-2"><i className="fas fa-long-arrow-alt-right" ></i></button> 
                          </div>
                     </div>
                   </div>
