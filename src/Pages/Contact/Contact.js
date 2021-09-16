d28820a2d26d19fdbdee977e10e6ddadfae1be1e
Sara mahmoodi <74267458+Sara-M5@users.noreply.github.com>
2021-09-16 05:46:10 +0430

    Update Contact.js

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -15,13 +15,13 @@ const Contact = () => {
     
       <div className="row">
 
-          <div className="col-md-6 col-lg-7 ">
+          <div className="col-sm-12 col-lg-7 col-xl-6">
           <iframe className="map" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=London%20Eye%2C%20London%2C%20United%20Kingdom&amp;t=m&amp;z=10&amp;output=embed&amp;iwloc=near" title="%3$s" aria-label="%3$s"></iframe>
           </div>
 
-          <div className="col-md-6 col-lg-5">
+          <div className="col-sm-0 col-lg-5 col-xl-6">
         
-            <div className="text-center">
+            <div className="text-center mt-5">
               <h1>Get in touch</h1>
               <p>Nemo enim ipsam voluptatem quia voluptas</p>
             </div>
@@ -49,21 +49,21 @@ const Contact = () => {
           <div className="row text-center" >
 
 
-            <div className="cards"  style={{background:"white"}}>
-             <i className="contactIcon fas fa-envelope"></i>
+            <div className="cards mt-5"  style={{background:"white"}}>
+             <i className="contactIcon fas fa-mail-bulk"></i>
              <h3 className="card-title mt-5">Email Address</h3>
              <p className="card-text">info@example.com</p>
             </div>
             
          
-             <div className="cards" style={{background:"white"}}>
-             <i className="contactIcon fas fa-phone"></i>
+             <div className="cards mt-5" style={{background:"white"}}>
+             <i className="contactIcon fas fa-phone-volume"></i>
              <h3 className="card-title mt-5">Phone Number</h3>
              <p className="card-text">+93 5676432976</p>
             </div>
 
 
-            <div className="cards" style={{background:"white"}}>
+            <div className="cards mt-5" style={{background:"white"}}>
              <i className="contactIcon fas fa-map-marked-alt"></i>
              <h3 className="card-title mt-5">Office Location</h3>
              <p className="card-text">202 New Hampshir Avenu, Kabul</p>
