544f3db6a0d95c98d1969553fa1323e8550846d6
Sara mahmoodi <74267458+Sara-M5@users.noreply.github.com>
2021-09-17 05:34:05 +0430

    Update Description.js

--- src/Component/Description/Description.js
+++ src/Component/Description/Description.js
@@ -2,18 +2,29 @@ import React from 'react';
 import './Description.css'
 
 const Description = (props) => {
-    return (
-
-        <div class="desc">
-      
-      
-                
-            <img className="info" src={props.images} />
-            <h2 className="total">{props.amount}</h2>
-            <p>{props.text}</p> 
-            
-
-             </div>
+    return (  
+         <div class="row text-center mt-5">
+                <div className="counter-icon col-md-3 mb-3">
+                   <i className="envira1-icon fab fa-envira" ></i>
+                   <h2 className="total">30</h2>
+                   <p>Total Items</p>
+                </div>
+                <div className="counter-icon col-md-3 mb-3">
+                   <i className="store-icon fab fa-app-store-ios" ></i>
+                   <h2 className="total">49</h2>
+                   <p>Total Sells</p>
+                </div>
+                <div className="counter-icon col-md-3 mb-3">
+                   <i className="user-icon fas fa-user-friends" ></i>
+                   <h2 className="total">104</h2>
+                   <p>Customers</p>
+                </div> 
+                <div className="counter-icon col-md-3 mb-3">
+                   <i className="envira2-icon fab fa-envira" ></i>
+                   <h2 className="total">54</h2>
+                   <p>Rating</p>
+                </div>  
+          </div>
         
        )};
    
